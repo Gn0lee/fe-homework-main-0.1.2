ee251b9c298fb7255e9271a495e3bb57d8437dfb
Gn0lee <jinho605@gmail.com>
2024-10-14 16:59:46 +0900

    CHORE: 필요 라이브러리 설치 및 worker start 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,37 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { SetupWorkerApi } from "msw/browser";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-if (process.env.NODE_ENV === "development") {
-  const { worker }: { worker: SetupWorkerApi } = require("./mocks/browser");
-  worker.start({ onUnhandledRequest: "bypass" });
-}
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const startApp = async () => {
+  if (process.env.NODE_ENV === "development") {
+    const { worker }: { worker: SetupWorkerApi } = require("./mocks/browser");
+    await worker.start({ onUnhandledRequest: "bypass" });
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement,
+  );
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+};
+
+startApp();
