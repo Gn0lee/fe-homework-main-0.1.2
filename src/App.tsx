dcaaeb77a4a9ec2e5e6f53757ef7d5b353bfa3d9
Gn0lee <jinho605@gmail.com>
2024-10-14 17:20:29 +0900

    CHORE: mui 설치 및 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Container, Grid2, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ padding: "2rem", width: "100%", height: "100%" }}>
+      <Grid2 container>
+        <Typography variant="h1" style={{ fontSize: "24px", fontWeight: 600 }}>
+          Your Fleet
+        </Typography>
+      </Grid2>
+    </Container>
   );
 }
 
