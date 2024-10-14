aebda15b3c37270fb695fe09c99fdd87c3ff824c
Gn0lee <jinho605@gmail.com>
2024-10-14 16:05:19 +0900

    FEAT: mock db 요소 추가

--- src/mocks/db.ts
+++ src/mocks/db.ts
@@ -1,29 +1,25 @@
+import { faker } from "@faker-js/faker";
+
 export interface Location {
   id: number;
   name: string;
   robot: {
     id: string;
     is_online: boolean;
-  };
+  } | null;
 }
 
-export const locations: Location[] = [
-  // Please add more locations to show features
-
-  {
-    id: 0,
-    name: "Spicy restaurant",
-    robot: {
-      id: "abcde123",
-      is_online: true,
-    },
-  },
-  {
-    id: 1,
-    name: "Salty restaurant",
-    robot: {
-      id: "fghij456",
-      is_online: false,
-    },
-  },
-];
+export const locations: Location[] = Array.from(
+  { length: 100 },
+  (_, index) => ({
+    id: index,
+    name: faker.company.name(),
+    robot:
+      Math.random() > 0.5
+        ? {
+            id: faker.string.alphanumeric(8),
+            is_online: faker.datatype.boolean(),
+          }
+        : null,
+  }),
+);
